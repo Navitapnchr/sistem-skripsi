af6079a059e4bbd329e1a0ea721b5ac23208d35f
Navitapnchr <navitapuan6@gmail.com>
2026-06-29 21:40:09 +0700

    daftar url docs

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('skripsi.urls')), # Ini endpoint API Anda
+    path('api/', include('skripsi.urls')), # Pastikan ini sesuai strukturmu
+    
+    # URL untuk tampilan cantik Swagger
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
