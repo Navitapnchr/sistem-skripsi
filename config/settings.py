87629f7da1369ad8314a03fb510bc14727a1ea59
Navitapnchr <navitapuan6@gmail.com>
2026-06-30 18:08:22 +0700

    add gunicorn and whitenoise for production

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,7 @@ SPECTACULAR_SETTINGS = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -101,5 +102,5 @@ USE_TZ = True
 
 # Static files
 STATIC_URL = 'static/'
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
--- skripsi/urls.py
+++ skripsi/urls.py
@@ -1,10 +1,6 @@
-from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import SkripsiViewSet
 
 router = DefaultRouter()
 router.register(r'skripsi', SkripsiViewSet)
-
-urlpatterns = [
-    path('', include(router.urls)),
-]
+urlpatterns = router.urls
